02aa07b745bc3aabc58ef150eeed226f933fa08b
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-05-31 19:20:08 +0800

    修复docker容器的端口访问不到web页的问题
    fix bug
    https://github.com/wukongdaily/AutoBuildImmortalWrt/issues/139
    https://github.com/wukongdaily/AutoBuildImmortalWrt/issues/112

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -91,6 +91,28 @@ elif [ "$count" -gt 1 ]; then
    fi
 fi
 
+# 添加docker zone
+uci add firewall zone
+uci set firewall.@zone[-1].name='docker'
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+uci set firewall.@zone[-1].device='docker0'
+
+# 添加 forwarding docker -> lan
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='docker'
+uci set firewall.@forwarding[-1].dest='lan'
+
+# 添加 forwarding docker -> wan
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='docker'
+uci set firewall.@forwarding[-1].dest='wan'
+
+# 添加 forwarding lan -> docker
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='lan'
+uci set firewall.@forwarding[-1].dest='docker'
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
