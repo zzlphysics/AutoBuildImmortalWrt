bd263f4dc4d6e6cb363ece93eee1579c02ce1b9a
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-07-17 17:12:17 +0800

    Create custom-packages.sh

---
+++ shell/custom-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ============= imm仓库外的第三方插件==============
+# ============= 若启用 则打开注释 ================
+# istore商店
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-store"
+# 首页和网络向导
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+# 去广告adghome
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+# 代理相关
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ssr-plus"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall2"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
+# VPN
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-tailscale-zh-cn"
+# 分区扩容 by sirpdboy 
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-partexp-zh-cn"
+# 酷猫主题 by sirpdboy 
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-kucat"
+# 网络测速 by sirpdboy 
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-netspeedtest-zh-cn"
+# MosDNS
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-mosdns-zh-cn"
