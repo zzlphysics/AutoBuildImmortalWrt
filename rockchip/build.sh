d91208ec7a4313b67daffab477e4407c8f83702f
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-07-18 16:06:49 +0800

    修改自定义软件包的位置

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Log file for debugging
+source shell/custom-packages.sh
+echo "第三方软件包: $CUSTOM_PACKAGES"
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # yml 传入的路由器型号 PROFILE
@@ -20,23 +22,28 @@ EOF
 echo "cat pppoe-settings"
 cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 
-# 下载 run 文件仓库
-echo "🔄 Cloning run file repo..."
-git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+if [ -z "$CUSTOM_PACKAGES" ]; then
+  echo "⚪️ 未选择 任何第三方软件包"
+else
+  # 下载 run 文件仓库
+  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
+  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
 
-# 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
-mkdir -p /home/build/immortalwrt/extra-packages
-cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
+  # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
+  mkdir -p /home/build/immortalwrt/extra-packages
+  cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
 
-echo "✅ Run files copied to extra-packages:"
-ls -lh /home/build/immortalwrt/extra-packages/*.run
-# 解压并拷贝ipk到packages目录
-sh prepare-packages.sh
-ls -lah /home/build/immortalwrt/packages/
-# 添加架构优先级信息
-sed -i '1i\
-arch aarch64_generic 10\n\
-arch aarch64_cortex-a53 15' repositories.conf
+  echo "✅ Run files copied to extra-packages:"
+  ls -lh /home/build/immortalwrt/extra-packages/*.run
+  # 解压并拷贝ipk到packages目录
+  sh shell/prepare-packages.sh
+  ls -lah /home/build/immortalwrt/packages/
+  # 添加架构优先级信息
+  sed -i '1i\
+  arch aarch64_generic 10\n\
+  arch aarch64_cortex-a53 15' repositories.conf
+fi
+
 
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
@@ -61,30 +68,9 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
-
-# ============= imm仓库外的第三方插件===============
-# ===== 若启用 请打开PACKAGES前面的注释==============
-# istore商店
-#PACKAGES="$PACKAGES luci-app-store"
-# 首页和网络向导
-#PACKAGES="$PACKAGES luci-i18n-quickstart-zh-cn"
-# 去广告adghome
-#PACKAGES="$PACKAGES luci-app-adguardhome"
-# 代理相关
-#PACKAGES="$PACKAGES luci-app-ssr-plus"
-#PACKAGES="$PACKAGES luci-app-passwall2"
-#PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
-# VPN
-#PACKAGES="$PACKAGES luci-app-tailscale"
-#PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
-# 分区扩容 by sirpdboy 
-#PACKAGES="$PACKAGES luci-app-partexp"
-#PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn"
-# 酷猫主题 by sirpdboy 
-#PACKAGES="$PACKAGES luci-theme-kucat"
-# 网络测速 by sirpdboy 
-#PACKAGES="$PACKAGES luci-app-netspeedtest"
-#PACKAGES="$PACKAGES luci-i18n-netspeedtest-zh-cn"
+# ======== shell/custom-packages.sh =======
+# 合并imm仓库以外的第三方插件
+PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
